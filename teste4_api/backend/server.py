218b9965469e7d428a0d53c5994ea266558a5fb3
Sabrina Souza <sabrinasouza.dev@gmail.com>
2025-03-28 13:57:19 -0300

    feat: filtrando todas as colunas e adicionando o limit padrao na consulta

--- teste4_api/backend/server.py
+++ teste4_api/backend/server.py
@@ -3,7 +3,7 @@ import pandas as pd
 from flask_cors import CORS
 
 
-df = pd.read_csv('./Relatorio_cadop.csv', encoding='utf-8', delimiter=';',keep_default_na=False )
+df = pd.read_csv('./Relatorio_cadop.csv', encoding='utf-8', delimiter=';',dtype=str ,keep_default_na=False )
 
 app = Flask(__name__)
 CORS(app)
@@ -12,9 +12,14 @@ CORS(app)
 @app.route('/buscar', methods=['GET'])
 def buscar():
     termo = request.args.get('query', '')
-    resultado = df[df['Nome_Fantasia'].str.contains(termo, case=False, na=False)].to_dict(orient='records')
+    limit = int(request.args.get('limit', 10))
 
-    return jsonify(resultado)
+    mask = df.apply(
+            lambda col: col.astype(str).str.lower().str.contains(termo, na=False)
+            ).any(axis=1)
+        
+    resultados = df[mask].head(limit).to_dict(orient='records')
+    return jsonify(resultados)
 
 if __name__ == '__main__':
     app.run(debug=True)
